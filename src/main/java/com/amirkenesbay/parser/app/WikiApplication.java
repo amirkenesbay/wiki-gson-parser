810915cbb01ae5f76189f3a91cd4daeda5f2f169
amirkenesbay <amirkenesbay@gmail.com>
2022-02-21 19:51:02 +0600

    Add regex to delete straight brackets and commas

--- src/main/java/com/amirkenesbay/parser/app/WikiApplication.java
+++ src/main/java/com/amirkenesbay/parser/app/WikiApplication.java
@@ -1,13 +1,12 @@
 package com.amirkenesbay.parser.app;
 
-import com.amirkenesbay.parser.parsers.FileParser;
 import com.amirkenesbay.parser.parsers.JsonParser;
 import com.amirkenesbay.parser.requests.HttpGetRequest;
 
 import java.io.IOException;
 
 public class WikiApplication {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String request = HttpGetRequest.getRequest();
         String response = HttpGetRequest.getResponseFromWiki(request);
         String result = JsonParser.parseResponse(response);
--- src/main/java/com/amirkenesbay/parser/parsers/JsonParser.java
+++ src/main/java/com/amirkenesbay/parser/parsers/JsonParser.java
@@ -8,20 +8,24 @@ import com.google.gson.JsonObject;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JsonParser {
     private final static List<Search> searches = new ArrayList<>();
+    private final static String FILE_NAME = "wiki.json";
+    private final static String OBJECT_QUERY = "query";
+    private final static String ARRAY_SEARCH = "search";
+    private final static String OBJECT_SNIPPET = "snippet";
+    private final static String SPAN_REGEX = "^\\[|\\]$|\\,|<.*?>";
+    private final static String EMPTY_STRING = "";
     private static void parseJsonFile(){
-        try (FileReader fileReader = new FileReader("wiki.json")) {
+        try (FileReader fileReader = new FileReader(FILE_NAME)) {
             JsonObject object = new Gson().fromJson(fileReader, JsonObject.class);
-            JsonArray jsonArrayOfSearch = object.getAsJsonObject("query").get("search").getAsJsonArray();
+            JsonArray jsonArrayOfSearch = object.getAsJsonObject(OBJECT_QUERY).get(ARRAY_SEARCH).getAsJsonArray();
             for (JsonElement searchElement : jsonArrayOfSearch) {
                 JsonObject searchJsonObject = searchElement.getAsJsonObject();
-                String snippet = searchJsonObject.get("snippet").getAsString();
+                String snippet = searchJsonObject.get(OBJECT_SNIPPET).getAsString();
                 Search search = new Search(snippet);
                 searches.add(search);
             }
@@ -31,7 +35,7 @@ public class JsonParser {
     }
 
     public static String showResult(String result){
-        return searches.toString().replaceAll("<.*?>", "");
+        return searches.toString().replaceAll(SPAN_REGEX, EMPTY_STRING);
 //        for (Search searchResult : searches) {
 //            result = searchResult.toString().replaceAll("<.*?>", "");
 //            System.out.println(result);
